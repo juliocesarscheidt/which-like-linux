cafb59cc8706c048cc29ad42a0b3f810e3b3075b
juliocesarscheidt <julio.scheidt96@gmail.com>
2023-02-16 19:08:21 -0300

    Add script

--- script_demo.ps1
+++ script_demo.ps1
@@ -2,7 +2,10 @@ function LogMe_AndDisplay([string]$logStr, [bool]$DoDisplay = $true)
 {
   Write-Output $logStr
 }
-LogMe_AndDisplay -logStr 'Hello World'
 
-write-host -ForegroundColor "Red"  "Sleeping 5 secs..."
+LogMe_AndDisplay -logStr $Env:Temp
+
+LogMe_AndDisplay -logStr 'Hello World' >> "$Env:Temp\output-$(date -f yyyy-MM-dd-hh-mm-ss).log"
+
+write-host -ForegroundColor "Red" "Sleeping 5 secs..."
 Start-Sleep 5
